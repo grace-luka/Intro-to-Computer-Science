b2b8f2df4e8a3230277bbe5be4ebed92a046ee8d
grace-luka <118309010+grace-luka@users.noreply.github.com>
2022-11-21 12:35:06 -0700

    Week12 - Function 1

---
+++ 102/Week12-utility.py
@@ -0,0 +1,18 @@
+#   Grace Luka
+#  ​ CSCI 102 – Section B
+#   Assessment 12C
+#   References:
+#   Time: 45 minutes
+
+
+# Function 1 ---------------------------
+
+def load_file(filename):
+    with open(filename, 'r') as myFile:
+        lines = []
+        contents = myFile.readlines()
+        for i in contents:
+            i = i.strip()
+            lines.append(i)
+    return lines
+
